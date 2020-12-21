3b9e626070ef8adee17a8787cde6972d75a74f5a
Radosław Pawluczuk <radoslaw.pawluczuk@sollers.eu>
2020-12-21 11:08:52 +0100

    Joke edition against of Joke details

--- src/app/jokes/jokes-list/jokes-list.component.ts
+++ src/app/jokes/jokes-list/jokes-list.component.ts
@@ -64,7 +64,7 @@ export class JokesListComponent implements OnInit {
     });
   }
 
-  goToJokeDetails(joke: Joke) {
+  goToJokeEdition(joke: Joke) {
     this.router.navigate(['/jokes', joke.id]);
   }
 
