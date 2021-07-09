4b37d1e4e98b3db2eb5e9acae6b2fafd88ef2bfd
Radosław Pawluczuk <rmpawluczuk@gmail.com>
2021-07-09 15:57:07 +0200

    All section works fine for filtering by author

--- src/app/jokes/jokes-main-view/jokes-main-view.component.ts
+++ src/app/jokes/jokes-main-view/jokes-main-view.component.ts
@@ -11,93 +11,92 @@ import {Pagination} from '../../utils/pagination';
 import {PaginationService} from '../../utils/pagination.service';
 
 @Component({
-    selector: 'app-jokes-main-view',
-    templateUrl: './jokes-main-view.component.html',
-    styleUrls: ['./jokes-main-view.component.css']
+  selector: 'app-jokes-main-view',
+  templateUrl: './jokes-main-view.component.html',
+  styleUrls: ['./jokes-main-view.component.css']
 })
 export class JokesMainViewComponent implements OnInit {
 
-    pagination: Pagination = new Pagination();
+  pagination: Pagination = new Pagination();
 
-    jokes: Joke[];
-    authors: Author[];
-    origins: Origin[];
-    allStructures: Structure[] = [];
+  jokes: Joke[];
+  authors: Author[];
+  origins: Origin[];
+  allStructures: Structure[] = [];
 
-    authorFilter = -1;
+  authorFilter;
 
-    constructor(private jokesService: JokesService,
-                private structuresService: StructuresService,
-                private authorsService: AuthorsService,
-                private originService: OriginService,
-                private paginationService: PaginationService
-    ) {
-    }
+  constructor(private jokesService: JokesService,
+              private structuresService: StructuresService,
+              private authorsService: AuthorsService,
+              private originService: OriginService,
+              private paginationService: PaginationService
+  ) {
+  }
 
-    ngOnInit(): void {
-        this.loadStructures();
-        this.loadAuthors();
-        this.loadOrigins();
-        this.loadJokes();
-    }
+  ngOnInit(): void {
+    this.loadStructures();
+    this.loadAuthors();
+    this.loadOrigins();
+    this.loadJokes();
+  }
 
-    loadPagination(): void {
-        this.paginationService.getPagination()
-            .subscribe(pagination => {
-                this.pagination = pagination;
-                this.pagination.currentPage += 1;   // difference between backend and fronted
-            });
-    }
-
-    loadJokes(): void {
-        this.jokesService.getAllJokes()
-            .subscribe(jokes => {
-                this.jokes = jokes;
-                console.log(jokes);
-                this.loadPagination();
-            });
-    }
-
-    loadFilteredJokes(pagination: Pagination, authorFilter: number): void {
-        this.jokesService.getFilteredJokes(authorFilter)
-            .subscribe(jokes => {
-                this.jokes = jokes;
-            });
-    }
-
-    loadStructures(): void {
-        this.structuresService.getStructures().subscribe((structures) => {
-            this.allStructures = structures;
-        });
-    }
-
-    loadAuthors(): void {
-        this.authorsService.getAuthors().subscribe((authors) => {
-            this.authors = authors;
-        });
-    }
-
-    loadOrigins(): void {
-        this.originService.getOrigins().subscribe((origins) => {
-            this.origins = origins;
-        });
-    }
-
-    filterJokesByAuthor(authorId: number) {
-        this.pagination.currentPage = 0;
-        this.loadFilteredJokes(this.pagination, authorId);
-    }
-
-    onRemovedJoke(jokeId: number) {
-        this.jokesService.removeJoke(jokeId).subscribe(() => {
-            this.pagination.currentPage = 0;
-            this.loadJokes();
-        });
-    }
-
-    updatePagination(pagination: Pagination) {
+  loadPagination(): void {
+    this.paginationService.getPagination()
+      .subscribe(pagination => {
         this.pagination = pagination;
-        this.paginationService.updatePagination(this.pagination)
-            .subscribe(() => this.loadJokes());
+        this.pagination.currentPage += 1;   // difference between backend and fronted
+      });
+  }
+
+  loadJokes(): void {
+    this.jokesService.getAllJokes()
+      .subscribe(jokes => {
+        this.jokes = jokes;
+        this.loadPagination();
+      });
+  }
+
+  filterJokesByAuthor(authorFilter) {
+    this.pagination.currentPage = 0;
+    if (authorFilter === 'All') {
+      this.loadJokes();
+    } else {
+      this.jokesService.getFilteredJokes(authorFilter)
+        .subscribe(jokes => {
+          this.jokes = jokes;
+        });
     }
+  }
+
+  loadStructures(): void {
+    this.structuresService.getStructures().subscribe((structures) => {
+      this.allStructures = structures;
+    });
+  }
+
+  loadAuthors(): void {
+    this.authorsService.getAuthors().subscribe((authors) => {
+      this.authors = authors;
+    });
+  }
+
+  loadOrigins(): void {
+    this.originService.getOrigins().subscribe((origins) => {
+      this.origins = origins;
+    });
+  }
+
+  onRemovedJoke(jokeId: number) {
+    this.jokesService.removeJoke(jokeId).subscribe(() => {
+      this.pagination.currentPage = 0;
+      this.loadJokes();
+    });
+  }
+
+  updatePagination(pagination: Pagination) {
+    this.pagination = pagination;
+    this.paginationService.updatePagination(this.pagination)
+      .subscribe(() => this.loadJokes());
+  }
 }
